14fe959be008d1033292f51743bb8215f8b0b6a1
CarlosCente <31189121+CarlosCente@users.noreply.github.com>
2020-01-12 20:21:32 +0100

    Cambios en el modal
    Primeros cambios en el modal, se añaden los 3 campos y se ponen como invisibles. Falta el control javascript para que se muestre el que pertenece a cada opcion del select

---
+++ src/main/java/com/cjhercen/springboot/app/models/object/IncidenciaFichaje.java
@@ -0,0 +1,17 @@
+package com.cjhercen.springboot.app.models.object;
+
+public class IncidenciaFichaje {
+	
+	private String tipo;
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
