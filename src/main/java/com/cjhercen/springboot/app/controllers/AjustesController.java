fefb05e7303b904cecc26b0d34a5223cc8b1aee2
CarlosCente <31189121+CarlosCente@users.noreply.github.com>
2020-02-11 15:15:39 +0100

    cambio contraseña

--- src/main/java/com/cjhercen/springboot/app/controllers/AjustesController.java
+++ src/main/java/com/cjhercen/springboot/app/controllers/AjustesController.java
@@ -1,19 +1,59 @@
 package com.cjhercen.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.cjhercen.springboot.app.models.entity.Usuario;
+import com.cjhercen.springboot.app.models.object.FormCambioPassword;
+import com.cjhercen.springboot.app.models.service.impl.UsuarioServiceImpl;
 
 @Controller
 public class AjustesController {
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	UsuarioServiceImpl usuarioService;
+	
 	@GetMapping("/ajustes")
 	public String ajustes(Model model, RedirectAttributes flash) {
 		
+		FormCambioPassword formCambioPassword = new FormCambioPassword();
+		model.addAttribute("formCambioPassword", formCambioPassword);
 		model.addAttribute("titulo", "Configuraciones disponibles");
 		return "ajustes";
 	}
 	
+	
+	@RequestMapping("/ajustes/cambioPassword")
+	public String cambioPassword(Model model, RedirectAttributes flash,
+			@ModelAttribute("formCambioPassword") FormCambioPassword formCambioPassword) {
+		
+		Usuario usuarioConectado = usuarioService.findByUsername(usuarioService.getUsername());
+		String passwordActual = usuarioConectado.getPassword();
+		
+		//Se compara el password introducido como actual para ver si conincide con la contraseña del usuario
+		if(passwordEncoder.matches(formCambioPassword.getActual(), passwordActual)) {
+			
+			//Se comparan las dos contraseñas nuevas para ver si coinciden
+			if(formCambioPassword.getNueva().equals(formCambioPassword.getConfirmacion())) {
+				
+			} else {
+				//Añadir ventana de error con la password de confirmación
+			}
+			
+		} else {
+			//Añadir ventana de error
+		}
+		
+		return "redirect:/ajustes";
+	}
 	
 }
---
+++ src/main/java/com/cjhercen/springboot/app/models/object/FormCambioPassword.java
@@ -0,0 +1,37 @@
+package com.cjhercen.springboot.app.models.object;
+
+public class FormCambioPassword {
+
+	private String actual;
+	
+	private String nueva;
+	
+	private String confirmacion;
+
+	public String getActual() {
+		return actual;
+	}
+
+	public void setActual(String actual) {
+		this.actual = actual;
+	}
+
+	public String getNueva() {
+		return nueva;
+	}
+
+	public void setNueva(String nueva) {
+		this.nueva = nueva;
+	}
+
+	public String getConfirmacion() {
+		return confirmacion;
+	}
+
+	public void setConfirmacion(String confirmacion) {
+		this.confirmacion = confirmacion;
+	}
+	
+	
+	
+}
